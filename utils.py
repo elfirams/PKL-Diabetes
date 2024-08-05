a5d05072d27fb9522afd26113f15ad930f13fa36
elfirams <160338735+elfirams@users.noreply.github.com>
2024-08-05 08:17:11 +0700

    Add files via upload

---
+++ utils.py
@@ -0,0 +1,11 @@
+import numpy as np
+import pandas as pd
+import re
+import pickle
+import streamlit as st
+import sklearn
+
+@st.cache(ttl=12500, allow_output_mutation=True)
+def load_model():
+    model = pickle.load(open(r"C:\Users\HP\Documents\PKL\nb_diabetes\naive_bayes_model.pkl", "rb"))
+    return model
